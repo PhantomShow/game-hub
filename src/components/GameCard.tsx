522e7cc024d0fe05443b18035fb8fdc812279ef8
PhantomShow <gonzalezjohn9983@yahoo.com>
2023-04-20 20:00:58 -0400

    Display metacritic score

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -4,11 +4,13 @@ import {
   Card,
   CardBody,
   CardHeader,
+  HStack,
   Heading,
   Image,
   Text,
 } from "@chakra-ui/react";
 import PlatformIconList from "./PlatformIconList";
+import CriticScore from "./CriticScore";
 
 interface Props {
   game: Game;
@@ -20,9 +22,12 @@ const GameCard = ({ game }: Props) => {
       <Image src={game.background_image} />
       <CardBody>
         <Heading fontSize="4xl">{game.name}</Heading>
-        <PlatformIconList
-          platforms={game.parent_platforms.map((p) => p.platform)}
-        />
+        <HStack justifyContent="space-between">
+          <PlatformIconList
+            platforms={game.parent_platforms.map((p) => p.platform)}
+          />
+          <CriticScore score={game.metacritic} />
+        </HStack>
       </CardBody>
     </Card>
   );
