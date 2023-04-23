0198beebefc8f5a31b39a2e249461ae6301c0257
PhantomShow <gonzalezjohn9983@yahoo.com>
2023-04-23 18:27:07 -0400

    Implement searching

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,7 @@ import SortSelector from "./components/SortSelector";
 export interface GameQuery {
   genre: Genre | null;
   platform: Platform | null;
+  searchText: string;
   sortOption: string;
 }
 
@@ -29,7 +30,9 @@ function App() {
       }}
     >
       <GridItem area={"nav"}>
-        <NavBar />
+        <NavBar
+          onSearch={(searchText) => setGameQuery({ ...gameQuery, searchText })}
+        />
       </GridItem>
       <Show above="lg">
         <GridItem area={"aside"} paddingX={5}>
