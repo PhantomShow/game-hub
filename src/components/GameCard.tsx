1c28e513d60845086d8d747480dfb526f0cc6a09
PhantomShow <gonzalezjohn9983@yahoo.com>
2023-04-24 16:30:16 -0400

    Clean up game cards

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -22,13 +22,13 @@ const GameCard = ({ game }: Props) => {
     <Card>
       <Image src={getCroppedImageUrl(game.background_image)} />
       <CardBody>
-        <Heading fontSize="4xl">{game.name}</Heading>
-        <HStack justifyContent="space-between">
+        <HStack marginBottom={3} justifyContent="space-between">
           <PlatformIconList
             platforms={game.parent_platforms.map((p) => p.platform)}
           />
           <CriticScore score={game.metacritic} />
         </HStack>
+        <Heading fontSize="4xl">{game.name}</Heading>
       </CardBody>
     </Card>
   );
