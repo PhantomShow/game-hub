46aadb735ce5030cbec50025e49b02830ebbf159
PhantomShow <gonzalezjohn9983@yahoo.com>
2023-04-27 19:18:29 -0400

    Refactor: Relocate platform interface

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,7 +1,12 @@
 import { useQuery } from "@tanstack/react-query";
 import apiClient from "../services/api-client";
 import { FetchResponse } from "../services/api-client";
-import { Platform } from "./useGames";
+
+export interface Platform {
+  id: number;
+  name: string;
+  slug: string;
+}
 
 // const usePlatforms = () => useData<Platform>("/platforms/lists/parents");
 const usePlatforms = () =>
