3cb8713ffa48c3854329a6b02d1793066b220136
PhantomShow <gonzalezjohn9983@yahoo.com>
2023-04-27 20:56:08 -0400

    Refactor: Create lookup  hooks

---
+++ src/hooks/usePlatform.ts
@@ -0,0 +1,10 @@
+import React from "react";
+import usePlatforms from "./usePlatforms";
+import { GameQuery } from "../App";
+
+const usePlatform = (id?: number) => {
+  const { data: platforms } = usePlatforms();
+  return platforms?.results.find((p) => p.id === id);
+};
+
+export default usePlatform;
