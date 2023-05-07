2179efc56930e09b6155970466a9b15cffebeeba
PhantomShow <gonzalezjohn9983@yahoo.com>
2023-05-07 19:15:42 -0400

    Build game attributes

--- src/entities/Game.ts
+++ src/entities/Game.ts
@@ -1,4 +1,6 @@
+import { Genre } from "./Genre";
 import { Platform } from "./Platform";
+import { Publisher } from "./Publisher";
 
 export interface Game {
   description_raw: string;
@@ -8,5 +10,7 @@ export interface Game {
   background_image: string;
   parent_platforms: { platform: Platform }[];
   metacritic: number;
+  publishers: Publisher[];
   rating_top: number;
+  genres: Genre[];
 }
