0d37bf0a706efbf145f3150901674d98a8ee2d26
PhantomShow <gonzalezjohn9983@yahoo.com>
2023-05-07 19:58:54 -0400

    Improve layout and navbar

--- src/components/GameScreenshots.tsx
+++ src/components/GameScreenshots.tsx
@@ -13,7 +13,7 @@ const GameScreenshots = ({ gameId }: Props) => {
   if (error) throw error;
 
   return (
-    <SimpleGrid columns={{ base: 1, md: 2, lg: 3 }} spacing={2}>
+    <SimpleGrid columns={{ base: 1, md: 2 }} spacing={2}>
       {data?.results?.map((s) => (
         <Image src={s.image} key={s.id} />
       ))}
